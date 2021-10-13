c56837aa18008a998e4a910614725771d0c7c869
Jishnu M <jishnu@upsquad.in>
2021-10-14 01:45:36 +0530

    changes maded

--- lib/main.dart
+++ lib/main.dart
@@ -69,73 +69,80 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Query(
-        options: QueryOptions(
-          document: gql(productsGraphQL),
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-        builder: (QueryResult result, {fetchMore, refetch}) {
-          if (result.hasException) {
-            return Text(result.exception.toString());
-          }
+        body: Query(
+          options: QueryOptions(
+            document: gql(productsGraphQL),
+          ),
+          builder: (QueryResult result, {fetchMore, refetch}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
 
-          if (result.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+            if (result.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-          final productList = result.data?['products']['edges'];
-          // print(productList);
+            final productList = result.data?['products']['edges'];
+            // print(productList);
 
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  "Products",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 2.0,
-                    crossAxisSpacing: 2.0,
-                    childAspectRatio: 0.75,
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Products",
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                  itemCount: productList.length,
-                  itemBuilder: (_, index) {
-                    var product = productList[index]['node'];
-
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(2.0),
-                          width: 180,
-                          height: 180,
-                          child: Image.network(product['thumbnail']['url']),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Text(
-                            product['name'],
-                            style: Theme.of(context).textTheme.subtitle2,
-                          ),
-                        ),
-                        const Text("\$4.50")
-                      ],
-                    );
-                  },
                 ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 2.0,
+                      crossAxisSpacing: 2.0,
+                      childAspectRatio: 0.75,
+                    ),
+                    itemCount: productList.length,
+                    itemBuilder: (_, index) {
+                      var product = productList[index]['node'];
+
+                      return Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(2.0),
+                            width: 180,
+                            height: 180,
+                            child: Image.network(product['thumbnail']['url']),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text(
+                              product['name'],
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                          ),
+                          const Text("\$4.50")
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.send),
+          ),
+        ));
   }
 }
